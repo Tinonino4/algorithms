9bc7c46d7e775838e77d7b1f557dbb39c6ec4d37
Tino <ahgalonso@gmail.com>
2023-12-11 15:18:50 +0100

    NQueens solved

--- src/main/kotlin/nqueens/Main.kt
+++ src/main/kotlin/nqueens/Main.kt
@@ -15,35 +15,41 @@ fun main() {
 }
 
 fun solveNQueens(): List<List<String>> {
-    println("N queens problem start:")
-    val chessBoard= Array(nQueens) { CharArray(nQueens) {'.'} }
     var solutions = mutableListOf<List<String>>()
+    val chessBoard= Array(nQueens) { CharArray(nQueens) {'X'} }
     backtrack(0, chessBoard, solutions)
     return solutions
 }
 
-fun backtrack(col: Int, chessBoard: Array<CharArray>, solutions: MutableList<List<String>>) {
-    if (col >= chessBoard.size) {
+fun backtrack(row: Int, chessBoard: Array<CharArray>, solutions: MutableList<List<String>>) {
+    if (row == chessBoard.size) {
         solutions.add(chessBoard.map { String(it) })
         return
     }; // Finished!
 
-    for (i in 0..< nQueens) {
-        if (isSafeForQueen(i, col, chessBoard)) {
-            chessBoard[i][col] = 'Q'
-            backtrack(col + 1, chessBoard, solutions)
-            chessBoard[i][col] = '.' // No solution, get back
+    for (col in chessBoard.indices) {
+        if (isSafeForQueen(row, col, chessBoard)) {
+            chessBoard[row][col] = 'Q'
+            backtrack(row + 1, chessBoard, solutions)
+            chessBoard[row][col] = 'X' // No solution, get back
         }
     }
 }
 
 fun isSafeForQueen(row: Int, col: Int, chessBoard: Array<CharArray>): Boolean {
-    for (i in 0..< col) {
-        if (chessBoard[row][i] == 'Q') { // check if there is a queen in the row
-            return false
-        }
-        if (row - i >= 0 && chessBoard[row-i][col-i] == 'Q') return false
-        if ((row - i >= 0) && (col + i < nQueens) && chessBoard[row-i][col+i] == 'Q') return false
+    // check if there is a queen in the row
+    for (i in 0 until  row) {
+        if (chessBoard[i][col] == 'Q') return false
+    }
+    //check for queens in the upper left diagonal
+    for (i in row -1 downTo 0) {
+        val j = row - i + col
+        if (j >= 0 && j < chessBoard.size && chessBoard[i][j] == 'Q') return false
+    }
+    //check for queens in the upper right diagonal
+    for (i in row - 1 downTo 0) {
+        val j = col - (row - i)
+        if (j >= 0 && j < chessBoard.size && chessBoard[i][j] == 'Q') return false
     }
     return true
 }
